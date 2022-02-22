87ca986762736fb87e20cdfc2a6010b3e6b6d4c0
insecta75 <insecta75@yahoo.com.br>
2022-02-22 19:05:45 -0300

    Adicionei o projeto no Github

---
+++ README.md
@@ -0,0 +1,2 @@
+# Recriando a Interface do Netflix
+Repositório criado para o projeto de recriação da página inicial do Netflix. Com esse curso foi possível aprender a utilizar o CSS3, HTML5 e JQuery, e o conceito de responsividade em um site.
---
+++ js/owl/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:3
+        },
+        1200:{
+            items:5
+        }
+    }
+})
